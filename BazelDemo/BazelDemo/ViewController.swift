21ec50dfe82b1739ff9210097a23c14773679126
Sanju Naik <sanju.naik14@gmail.com>
2023-09-26 20:20:19 +0530

    Demo App to reproduce undefined symbol errors on dynamic framework

--- BazelDemo/BazelDemo/ViewController.swift
+++ BazelDemo/BazelDemo/ViewController.swift
@@ -6,7 +6,6 @@
 //
 
 import UIKit
-import FirebaseCrashlytics
 
 enum MyError: Error {
     case firstError
@@ -14,48 +13,9 @@ enum MyError: Error {
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        
+    override func viewDidLoad() {        
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-        testMethod()
-        let a = 4
-        print("a - \(a)")
-        let button = UIButton(type: .roundedRect)
-        button.frame = CGRect(x: 20, y: 50, width: 100, height: 30)
-        button.setTitle("Test Crash", for: [])
-        button.addTarget(self, action: #selector(self.crashButtonTapped(_:)), for: .touchUpInside)
-        view.addSubview(button)
-        
-//        let error = MyError.firstError
-//        Crashlytics.crashlytics().record(error: error)
+
     }
     
-    func testMethod() {
-        print("Testing")
-    }
-
-    @IBAction func crashButtonTapped(_ sender: AnyObject) {
-         let numbers = [0]
-         let _ = numbers[1]
-     }
-
 }
-
-//  --copt=-O0 \
-//   --copt=-g \
-//   --copt=-DDEBUG=1 \
-//   --copt=-fno-omit-frame-pointer  \
-//   --copt=-fobjc-arc \
-//   --copt=-no-canonical-prefixes \
-
-// --copt=-g0 \
-//   --copt=-O2 \
-//   --copt=-DNDEBUG \
-//   --copt=-DNS_BLOCK_ASSERTIONS=1 \
-//   --copt=-Os \
-//   --copt=-DNDEBUG=1 \
-//   --copt=-Wno-unused-variable \
-//   --copt=-Winit-self \
-//   --copt=-Wno-extra \
-//   --swiftcopt -g \
